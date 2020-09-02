5cad3c16b509738d885caf3b686753afa6343314
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-02 10:44:09 -0700

    begin build of chracter class
    > Co-authored-by:  Kolman Soifer  <kolmansoifer@gmail.com>

---
+++ src/js/Character.js
@@ -0,0 +1,12 @@
+export default class Character {
+  constructor() {
+    this.type =;
+    this.name =
+    this.inventory;
+    this.attributes;
+    this.hp;
+    this.damage;
+  }
+  hero(){}
+  enemy () {}
+}
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import { Character } from "src/js/Character.js";
 
 $(document).ready(function () {});
